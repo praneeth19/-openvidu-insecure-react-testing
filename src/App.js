91fcc3845928e4166c46498648f43acf4c90e4b8
praneeth19 <ppraneeth294@gmail.com>
2020-07-17 21:02:44 +0530

    video source false

--- src/App.js
+++ src/App.js
@@ -129,7 +129,7 @@ class App extends Component {
               // element: we will manage it on our own) and with the desired properties
               let publisher = this.OV.initPublisher(undefined, {
                 audioSource: undefined, // The source of audio. If undefined default microphone
-                videoSource: undefined, // The source of video. If undefined default webcam
+                videoSource: false, // The source of video. If undefined default webcam
                 publishAudio: true, // Whether you want to start publishing with your audio unmuted or not
                 publishVideo: false, // Whether you want to start publishing with your video enabled or not
                 resolution: "640x480", // The resolution of your video
